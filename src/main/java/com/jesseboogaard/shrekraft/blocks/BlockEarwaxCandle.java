d006ca1d17c0358d4418ef6b602da496aff52a45
Dennis Blokland <dennishoornaar@live.nl>
2018-03-12 19:23:35 +0100

    Fixed missing Texture fuse, Added Particles and fixed item model positions,

--- src/main/java/com/jesseboogaard/shrekraft/blocks/BlockEarwaxCandle.java
+++ src/main/java/com/jesseboogaard/shrekraft/blocks/BlockEarwaxCandle.java
@@ -3,9 +3,16 @@ package com.jesseboogaard.shrekraft.blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 public class BlockEarwaxCandle extends shrekraftBaseBlock{
     private final AxisAlignedBB box = new AxisAlignedBB(0.4000000059604645D, 0.0D, 0.4000000059604645D, 0.6000000238418579D, 0.4D, 0.6000000238418579D);
@@ -32,4 +39,14 @@ public class BlockEarwaxCandle extends shrekraftBaseBlock{
         return this.box;
     }
 
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        double d0 = (double) pos.getX() + 0.5D;
+        double d1 = (double) pos.getY() + 0.55D;
+        double d2 = (double) pos.getZ() + 0.5D;
+
+
+        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+    }
 }
