90fb19f37c0d31279153fab465ec03283eccc3bb
JesseBoogaard <jesse98@live.nl>
2018-03-13 21:44:36 +0100

    added 'peel' action on right click for onion.

--- src/main/java/com/jesseboogaard/shrekraft/items/ItemOnion.java
+++ src/main/java/com/jesseboogaard/shrekraft/items/ItemOnion.java
@@ -3,14 +3,19 @@ package com.jesseboogaard.shrekraft.items;
 import com.jesseboogaard.shrekraft.blocks.shrekraftBlocks;
 import com.jesseboogaard.shrekraft.shrekraft;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.*;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Random;
 
 
 public class ItemOnion extends ItemSeeds {
@@ -28,6 +33,18 @@ public class ItemOnion extends ItemSeeds {
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
     {
         tooltip.add("Onions have layers, so do Ogres.");
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer player, EnumHand hand) {
+
+        ItemStack itemstack = player.getHeldItem(hand);
+        if (!player.capabilities.isCreativeMode)
+        {
+            itemstack.shrink(1);
+        }
+            player.inventory.addItemStackToInventory(new ItemStack(shrekraftItems.ItemPeeledOnion, 1));
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
+
     }
 
     public void registerItemModel(Item item) {
